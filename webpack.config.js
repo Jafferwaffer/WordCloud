86ab03953dd8c74ab5c87ccf34924266cb832ced
Jafferwaffer <george.thomas@atomsupplies.com>
2018-03-08 18:15:27 +0000

    Folder restructure, add babel, dev web server

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/public');
-var APP_DIR = path.resolve(__dirname, 'src/app');
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'src/js');
 
 var config = {
   entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  devServer:{ contentBase: BUILD_DIR }
 };
 
 module.exports = config;
