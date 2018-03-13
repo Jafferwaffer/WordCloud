141c94d351bcdf98ff50966195501ef8a53ab3f4
Jafferwaffer <george.thomas@atomsupplies.com>
2018-03-13 18:19:45 +0000

    Polyfill for IE

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var APP_DIR = path.resolve(__dirname, 'src/js');
 
 var config = {
   devtool: 'cheap-module-eval-source-map',
-  entry: APP_DIR + '/index.js',
+  entry: ['babel-polyfill', APP_DIR + '/index.js'],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
